364f89e37c803eefe5765d9732dd7b26b8b9e601
James Martin Lucien Audretsch <jamaudre@silo.soic.indiana.edu>
2016-01-13 21:54:29 -0500

    added DNA file for lab1

---
+++ Lab1/RandomDNA.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class RandomDNA {
+  public static void main(String[] args){
+    dna_generate();
+    }
+  
+  public static String dna_generate(){
+    String result = "";
+    String letters = "actg";
+    Random rnd = new Random();
+
+    for (int i=1; i<21; i++){
+      int t = rnd.nextInt(4);
+      result += letters.charAt(t);
+      }
+    return result;
+  }
+}
